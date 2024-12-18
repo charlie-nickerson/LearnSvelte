0f587f99dd2171897dedb127fd4b4ebf7fb862c3
charlie-nickerson <charlie@nickerson.com>
2024-12-17 23:02:18 -0800

    Created a slider

---
+++ svelt-app/svelte-app/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Slider from "@bulatdashiev/svelte-slider";
+  let range = [0, 200];
+</script>
+
+Thumb order: left: {range[0]}
+right: {range[1]}
+<Slider max="200" step="10" bind:value={range} range order />
+
+<style>
+  div {
+    --thumb-bg: transparent;
+    --progress-bg: #ff9355;
+    --track-bg: #deffd7;
+  }
+</style>
